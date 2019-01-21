360f4bc153d061e649d0696e0baa167cba5ee680
Daniel Grijuela <danielgrijuela@gmail.com>
2019-01-21 11:51:57 +0100

    Fix types

--- app/Main.hs
+++ app/Main.hs
@@ -5,14 +5,13 @@ import System.Environment
 import Day1
 import Day2
 
--- runner :: Char Char -> IO ()
+runner :: Char -> Char -> IO ()
 runner '1' '1' = Day1.part1
 runner '1' '2' = Day1.part2
 runner '2' '1' = Day2.part1
 runner '2' '2' = Day2.part2
 
 main :: IO ()
--- main = part1
 main = do
   putStrLn "¿Qué día quieres?"
   day <- getLine
