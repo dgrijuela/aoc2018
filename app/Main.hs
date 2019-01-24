a8c1458d59bbb814bf056d3e94b2c40160048f1d
Daniel Grijuela <danielgrijuela@gmail.com>
2019-01-24 12:55:56 +0100

    Fuck haskell

--- app/Main.hs
+++ app/Main.hs
@@ -1,22 +1,27 @@
 module Main where
 
 import System.Environment
+import Data.Time
 
 import Day1
-import Day2
+--import Day2
 
-runner :: Char -> Char -> IO ()
+--runner :: Char -> Char -> IO ()
 runner '1' '1' = Day1.part1
-runner '1' '2' = Day1.part2
-runner '2' '1' = Day2.part1
-runner '2' '2' = Day2.part2
-runner _ _ = print "No implementado todavía"
+--runner '1' '2' = Day1.part2
+--runner '2' '1' = Day2.part1
+--runner '2' '2' = Day2.part2
+--runner x y = show ("Día " ++ (show x) ++ " parte " ++ (show y) ++ " no implementada todavía")
 
-main :: IO ()
+--main :: IO ()
 main = do
-  putStrLn "¿Qué día quieres?"
+  putStrLn "\n¿Qué día quieres?"
   day <- getLine
-  putStrLn "¿Qué parte quieres?"
+  putStrLn "\n¿Qué parte quieres?"
   part <- getLine
-  print "Buena eleccion, procesando..."
+  putStrLn "\nBuena eleccion, procesando..."
+  start <- getCurrentTime
   runner (head day) (head part)
+  --putStrLn $ "\nResultado:" ++ (runner (head day) (head part))
+  end <- getCurrentTime
+  putStrLn $ "\nTiempo: " ++ (show $ diffUTCTime end start)
